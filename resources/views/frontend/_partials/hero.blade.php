872c1c5d553c563134afcf97fb1312c02a87cbb8
muzahidul-maruf <marufmuzahidul@gmail.com>
2023-09-23 22:53:23 +0600

    about sao

--- resources/views/frontend/_partials/hero.blade.php
+++ resources/views/frontend/_partials/hero.blade.php
@@ -1,4 +1,4 @@
-<section id="hero" class="d-flex align-items-center" style="background: url('frontend_assets/common/hero/1.png')">
+<section id="hero" class="d-flex align-items-center" style="">
     <div class="container">
         <h1 class="text-white">Preserving the Past, <br> Protecting the Future</h1>
         <h2 class="text-white">Welcome to the Multipurpose Specialized Preservation Center</h2>
--- resources/views/frontend/pages/about.blade.php
+++ resources/views/frontend/pages/about.blade.php
@@ -4,15 +4,35 @@
     About us
 @endsection
 @section('content')
-    <main id="main">
-        <div class="container-fluid pt-5">
-            <div class="row">
-                <img src="{{ asset('frontend_assets/common/hero/about_hero.png') }}" alt=""
-                    class="img-fluid d-block w-100">
+    <main id="main" style="min-height: 90vh;">
+        <div class="inner_page_hero d-flex flex-column align-items-center ">
+            <img src="{{ asset('frontend_assets/common/hero/about_hero.png') }}" alt=""
+                class="img-fluid d-block w-100 position-relative">
+
+            <div class="text-center position-absolute"
+                style="background-color: #10125e; padding: 15px 0px; border-radius: 10px; width:40%">
+                <h4 class="text-white">ABOUT US</h4>
             </div>
         </div>
 
 
+
+        {{-- <div class="row justify-content-center">
+            <div class="col-6 text-center" style="background-color: #10125e; padding: 15px 0px; border-radius: 10px;">
+                <h4 class="text-white">ABOUT US</h4>
+            </div>
+        </div> --}}
+
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
     </main>
 
 
