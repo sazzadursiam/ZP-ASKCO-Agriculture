2650444140ccf0ea80e09369061924096e3a2fe7
sazzadursiam <sazzadur.swe@gmail.com>
2023-10-01 05:53:54 +0600

    slier

--- resources/views/frontend/_partials/hero.blade.php
+++ resources/views/frontend/_partials/hero.blade.php
@@ -1,7 +1,52 @@
-<section id="hero" class="d-flex align-items-center" style="">
+{{-- <section id="hero" class="d-flex align-items-center" style="">
     <div class="container">
         <h1 class="text-white">Preserving the Past, <br> Protecting the Future</h1>
         <h2 class="text-white">Welcome to the Multipurpose Specialized Preservation Center</h2>
-        {{-- <a href="#about" class="btn-get-started scrollto">Get Started</a> --}}
     </div>
+</section> --}}
+<section id="hero">
+    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"
+                aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"
+                aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"
+                aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="{{ asset('frontend_assets/common/hero/2.jpg') }}" class="d-block w-100" alt="...">
+                <div class="carousel-caption ">
+                    <h2>Welcome to ASKCO Multipurpose Specialized Preservation Centre Limited</h2>
+                    <p>Fruits & seed storage (DH) with processing</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="{{ asset('frontend_assets/common/hero/3.jpg') }}" class="d-block w-100" alt="...">
+                <div class="carousel-caption ">
+                    <h2>Welcome to ASKCO Multipurpose Specialized Preservation Centre Limited</h2>
+                    <p>Fruits & seed storage (DH) with processing</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="{{ asset('frontend_assets/common/hero/4.jpg') }}" class="d-block w-100" alt="...">
+                <div class="carousel-caption ">
+                    <h2>Welcome to ASKCO Multipurpose Specialized Preservation Centre Limited</h2>
+                    <p>Fruits & seed storage (DH) with processing</p>
+                </div>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"
+            data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions"
+            data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+
 </section>
--- resources/views/frontend/pages/contact.blade.php
+++ resources/views/frontend/pages/contact.blade.php
@@ -126,9 +126,10 @@
             </div>
 
             <div>
-                <iframe style="border:0; width: 100%; height: 350px;"
-                    src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621"
-                    frameborder="0" allowfullscreen></iframe>
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d964.6602282418689!2d90.32656171624025!3d23.891903604902108!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c37a9cf406e3%3A0x5c9cb92988b786a2!2sAskco%20MSP%20Centre%20Limited!5e0!3m2!1sen!2sbd!4v1696099237630!5m2!1sen!2sbd"
+                    width="100%" height="600" style="border:0;" allowfullscreen="" loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
         </section><!-- End Contact Section -->
 
